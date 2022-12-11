31e42430f0761aaf63fcb597ddbd9d099a7ebf88
herjatiaji <herjati.aji@mail.ugm.ac.id>
2022-12-11 16:58:25 +0700

    intial commit

--- app/src/main/java/com/pad1/padrumahbelajar/quiz/QuizActivity.java
+++ app/src/main/java/com/pad1/padrumahbelajar/quiz/QuizActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.pad1.padrumahbelajar.R;
 
-public class QuizActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-    }
-}
+//public class QuizActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_quiz);
+//    }
+//}
